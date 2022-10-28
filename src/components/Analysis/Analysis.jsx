bf931e73190da00ac22134a540107147c150d541
wewbR <tarikbouchaala43@gmail.com>
2022-10-28 14:10:29 +0100

    first commit

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -63,133 +63,142 @@ export function Analysis(props) {
     }
     return objectData;
   }
+  let dataE;
+  let optionsE;
+  let dataI;
+  let optionsI;
   //Expenses Chart:
-  const getLocalStorageE = JSON.parse(localStorage.getItem('expenses'));
-  let getLocalStorageELabel;
-
-  if (props.categorieAnalysisE == 0) {
-    getLocalStorageELabel = getLocalStorageE;
-  }
-  else {
-    getLocalStorageELabel = getLocalStorageE.filter(expense => expense.categorie == props.categorieAnalysisE);
-  }
-
-  let labelsD = [...new Set(getLocalStorageELabel.map(expense => getMyRightDate(expense.date)))];
-  const sumDatesValues = sumDatesByDay(labelsD, getLocalStorageE, props.categorieAnalysisE);
-  const dataToUse = [];
-  for (let i in sumDatesValues) {
-    dataToUse.push(sumDatesValues[i]);
-  }
-
-  labelsD.sort();
-
-  const dataE = {
-    labels: labelsD,
-    datasets: [
-      {
-        label: props.categorieObjetE.filter(categorie => categorie.id == props.categorieAnalysisE) == 0 ? "All Expense Categories" : props.categorieObjetE.filter(categorie => categorie.id == props.categorieAnalysisE)[0].libelle,
-        data: dataToUse,
-        borderColor: ['rgb(53,162,235)', 'rgb(255,26,104)', "rgb(255,159,64)", "rgb(0,206,86)"],
-        backgroundColor: ["rgba(53,162,235,0.4)", "rgba(255,26,104,0.4)", "rgba(255,159,64,0.4)", "rgba(0,206,86,0.4)"]
-      }
-    ]
-  };
-
-  const optionsE = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top"
-      },
-      title: {
-        display: true,
-        text: "Expenses Chart"
-      },
-      tooltip: {
-        yAlign: 'top',
-        displayColors: false,
-        padding: 10
-      }
-    },
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'day'
+  if(localStorage.getItem('expenses')!=null){
+    const getLocalStorageE = JSON.parse(localStorage.getItem('expenses'));
+    let getLocalStorageELabel;
+  
+    if (props.categorieAnalysisE == 0) {
+      getLocalStorageELabel = getLocalStorageE;
+    }
+    else {
+      getLocalStorageELabel = getLocalStorageE.filter(expense => expense.categorie == props.categorieAnalysisE);
+    }
+  
+    let labelsD = [...new Set(getLocalStorageELabel.map(expense => getMyRightDate(expense.date)))];
+    const sumDatesValues = sumDatesByDay(labelsD, getLocalStorageE, props.categorieAnalysisE);
+    const dataToUse = [];
+    for (let i in sumDatesValues) {
+      dataToUse.push(sumDatesValues[i]);
+    }
+  
+    labelsD.sort();
+  
+    dataE = {
+      labels: labelsD,
+      datasets: [
+        {
+          label: props.categorieObjetE.filter(categorie => categorie.id == props.categorieAnalysisE) == 0 ? "All Expense Categories" : props.categorieObjetE.filter(categorie => categorie.id == props.categorieAnalysisE)[0].libelle,
+          data: dataToUse,
+          borderColor: ['rgb(53,162,235)', 'rgb(255,26,104)', "rgb(255,159,64)", "rgb(0,206,86)"],
+          backgroundColor: ["rgba(53,162,235,0.4)", "rgba(255,26,104,0.4)", "rgba(255,159,64,0.4)", "rgba(0,206,86,0.4)"]
+        }
+      ]
+    };
+  
+    optionsE = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top"
+        },
+        title: {
+          display: true,
+          text: "Expenses Chart"
+        },
+        tooltip: {
+          yAlign: 'top',
+          displayColors: false,
+          padding: 10
         }
       },
-      y: {
-        beginAtZero: true
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day'
+          }
+        },
+        y: {
+          beginAtZero: true
+        }
       }
-    }
-  };
-
+    };
+  
+  }
   //Incomes Chart:
-  const getLocalStorageI = JSON.parse(localStorage.getItem('incomes'));
-  let getLocalStorageILabel;
+if(localStorage.getItem('incomes')!=null){
+const getLocalStorageI = JSON.parse(localStorage.getItem('incomes'));
+let getLocalStorageILabel;
 
-  if (props.categorieAnalysisI == 0) {
-    getLocalStorageILabel = getLocalStorageI;
-  }
-  else {
-    getLocalStorageILabel = getLocalStorageI.filter(income => income.categorie == props.categorieAnalysisI);
-  }
+if (props.categorieAnalysisI == 0) {
+  getLocalStorageILabel = getLocalStorageI;
+}
+else {
+  getLocalStorageILabel = getLocalStorageI.filter(income => income.categorie == props.categorieAnalysisI);
+}
 
-  let labelsDI = [...new Set(getLocalStorageILabel.map(income => getMyRightDate(income.date)))];
-  const sumDatesValuesI = sumDatesByDay(labelsDI, getLocalStorageI, props.categorieAnalysisI);
-  const dataToUseI = [];
-  for (let i in sumDatesValuesI) {
-    dataToUseI.push(sumDatesValuesI[i]);
-  }
+let labelsDI = [...new Set(getLocalStorageILabel.map(income => getMyRightDate(income.date)))];
+const sumDatesValuesI = sumDatesByDay(labelsDI, getLocalStorageI, props.categorieAnalysisI);
+const dataToUseI = [];
+for (let i in sumDatesValuesI) {
+  dataToUseI.push(sumDatesValuesI[i]);
+}
 
-  labelsDI.sort();
+labelsDI.sort();
 
-  const dataI = {
-    labels: labelsDI,
-    datasets: [
-      {
-        label: props.categorieObjetI.filter(categorie => categorie.id == props.categorieAnalysisI) == 0 ? "All Income Categories" : props.categorieObjetI.filter(categorie => categorie.id == props.categorieAnalysisI)[0].libelle,
-        data: dataToUseI,
-        borderColor: ['rgb(53,162,235)', 'rgb(255,26,104)'],
-        backgroundColor: ["rgba(53,162,235,0.4)", "rgba(255,26,104,0.4)"]
-      }
-    ]
-  };
+dataI = {
+  labels: labelsDI,
+  datasets: [
+    {
+      label: props.categorieObjetI.filter(categorie => categorie.id == props.categorieAnalysisI) == 0 ? "All Income Categories" : props.categorieObjetI.filter(categorie => categorie.id == props.categorieAnalysisI)[0].libelle,
+      data: dataToUseI,
+      borderColor: ['rgb(53,162,235)', 'rgb(255,26,104)'],
+      backgroundColor: ["rgba(53,162,235,0.4)", "rgba(255,26,104,0.4)"]
+    }
+  ]
+};
 
-  const optionsI = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top"
-      },
-      title: {
-        display: true,
-        text: "Incomes Chart"
-      },
-      tooltip: {
-        yAlign: 'top',
-        displayColors: false,
-        padding: 10
+optionsI = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top"
+    },
+    title: {
+      display: true,
+      text: "Incomes Chart"
+    },
+    tooltip: {
+      yAlign: 'top',
+      displayColors: false,
+      padding: 10
+    }
+  },
+  scales: {
+    x: {
+      type: 'time',
+      time: {
+        unit: 'day'
       }
     },
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'day'
-        }
-      },
-      y: {
-        beginAtZero: true
-      }
+    y: {
+      beginAtZero: true
     }
-  };
+  }
+};
+
+}
   
   
   return <div className={AnalysisCss.Analysis}>
     <div className={AnalysisCss.header}>Analysis</div>
     <div className={AnalysisCss.Charts}>
-      <div className={AnalysisCss.AnalysisExpenses}>
+      {localStorage.getItem('expenses')!=null?<div className={AnalysisCss.AnalysisExpenses}>
         <div className={AnalysisCss.headerChart}>
           <label htmlFor="categorie">Expenses Categorie:</label>
           <select onChange={e => props.filterCategorieAnalysisE(e)} type="text" name='categorie'>
@@ -198,8 +207,8 @@ export function Analysis(props) {
           </select>
         </div>
         <Bar data={dataE} options={optionsE} width={600} height={400} />
-      </div>
-      <div className={AnalysisCss.AnalysisIncomes}>
+      </div>:<h1>No Expenses Inserted Yet</h1>}
+      {localStorage.getItem('incomes')!=null?<div className={AnalysisCss.AnalysisIncomes}>
         <div className={AnalysisCss.headerChart}>
           <label htmlFor="categorie">Incomes Categorie:</label>
           <select onChange={e => props.filterCategorieAnalysisI(e)} type="text" name='categorie'>
@@ -208,7 +217,7 @@ export function Analysis(props) {
           </select>
         </div>
         <Bar data={dataI} options={optionsI} width={600} height={400} />
-      </div>
+      </div>:<h1>No Incomes Inserted Yet</h1>}
     </div>
 
   </div>;
